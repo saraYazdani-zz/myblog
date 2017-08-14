57d95537c5c39d2ccd673ff05b81beffabf16978
SaraYazdani <saryaz@outlook.com>
2017-08-14 16:26:32 -0400

    add relationship of users and blog, defined authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814170545) do
+ActiveRecord::Schema.define(version: 20170814201617) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170814170545) do
     t.boolean  "publish",    default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
